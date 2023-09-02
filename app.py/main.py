3c97cf388f1911fa42a5dff34f1938d8dc564ee7
Gudipati Vishnu Vardhan Reddy <vishnureddy228@gmail.com>
2023-09-03 00:51:03 +0530

    Update main.py

--- app.py/main.py
+++ app.py/main.py
@@ -1,12 +1,13 @@
 import webbrowser
 
-from taipy.gui import Gui, Markdown, notify, Html
+from taipy.gui import Gui, Markdown, notify, Html, navigate
 import login
 import dashboard
+import register
 
-value = 0
-logo = "./images/Homepage.png"
-
+value = " ©2023 EduEnv. All rights reserved.\n Disclaimer: This website is for informational purposes only and does not constitute professional educational advice.\nConsult with a qualified educator or institution for personalized guidance."
+logo = "./images/logo.png"
+icon = "./images/ICON.png"
 
 page1 = Html("app.py/index.html")
 
@@ -29,11 +30,22 @@ def on_change(state, var_name: str, var_value):
     ...
 
 
+def logins(state):
+    navigate(state, to="login")
+
+
+def image_click(state):
+    navigate(state, to="home", force=True)
+
+
 page = {
     "home": page1
 }
 
+
 if __name__ == "__main__":
-    gui = Gui(pages=page, css_file="/styles.css")
+    gui = Gui(pages=page)
     gui.add_page(name="login", page=login.page)
-    gui.run(title="EDUENV")
+    gui.add_page(name="register", page=register.page)
+    gui.add_page(name="dashboard", page=dashboard.page)
+    gui.run(title="EDUENV", watermark=" ", favicon=icon)
