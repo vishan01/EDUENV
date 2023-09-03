6cd0495dbf58efb1f2771a1fa6e50b432343aaca
Gudipati Vishnu Vardhan Reddy <vishnureddy228@gmail.com>
2023-09-03 13:46:09 +0530

    Update login.py

--- app.py/login.py
+++ app.py/login.py
@@ -1,23 +1,25 @@
 from taipy.gui import Gui, Html, notify, navigate
 import dashboard
+from pymongo import MongoClient
+import url
 
+client = MongoClient(
+    host=url.url)
+db = client["users"]
+collection = db["user_password"]
 Username = ""
 Password = ""
-page = Html(
-    """
-    <center>
-        <h2>Welcome to the Login Page</h2>
-        <p>Username</p>
-        <taipy:input>{Username}</taipy:input>
-        <p>Password</p>
-        <taipy:input password=True>{Password}</taipy:input>
-
-
-        <taipy:button on_action="submit">Submit</taipy:button>
-    </center>
-"""
-)
+page = Html("app.py/login.html")
 
 
 def submit(state):
-    navigate(state, to="dashboard")
+    details = collection.find_one({"user": state.Username})
+    if (details == None):
+        details = {"user": " "*100, "password": " "*100}
+    if (state.Username == details["user"] and state.Password == details["password"]):
+        if (details["type"] == "student"):
+            navigate(state, to="dashboard", force=True)
+        else:
+            navigate(state, to="dashboard1", force=True)
+    else:
+        navigate(state, to="login", force=True)
