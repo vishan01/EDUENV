7da4dbc28fb38c798a084ce475da6d560d130119
Gudipati Vishnu Vardhan Reddy <vishnureddy228@gmail.com>
2023-09-03 13:46:02 +0530

    Update main.py

--- app.py/main.py
+++ app.py/main.py
@@ -3,6 +3,7 @@ import webbrowser
 from taipy.gui import Gui, Markdown, notify, Html, navigate
 import login
 import dashboard
+import dashboard1
 import register
 
 value = " ©2023 EduEnv. All rights reserved.\n Disclaimer: This website is for informational purposes only and does not constitute professional educational advice.\nConsult with a qualified educator or institution for personalized guidance."
@@ -15,19 +16,6 @@ page1 = Html("app.py/index.html")
 def image_action(state):
     webbrowser.open(
         "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley")
-
-
-def on_push(state):
-    ...
-
-
-def on_slider(state):
-    if state.value == 100:
-        notify(state, "success", "Taipy is running!")
-
-
-def on_change(state, var_name: str, var_value):
-    ...
 
 
 def logins(state):
@@ -48,4 +36,5 @@ if __name__ == "__main__":
     gui.add_page(name="login", page=login.page)
     gui.add_page(name="register", page=register.page)
     gui.add_page(name="dashboard", page=dashboard.page)
+    gui.add_page(name="dashboard1", page=dashboard1.page)
     gui.run(title="EDUENV", watermark=" ", favicon=icon)
